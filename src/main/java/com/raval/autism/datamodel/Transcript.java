a6bb955b30118dcb161b114c995c8433dc287cbf
Valentin Moreno <valenmope@gmail.com>
2011-10-07 14:05:57 +0200

    add getters and setters

--- src/main/java/com/raval/autism/datamodel/Transcript.java
+++ src/main/java/com/raval/autism/datamodel/Transcript.java
@@ -9,4 +9,20 @@ public class Transcript extends AuditObject {
 	private Student student;
 
 	private List<Qualification> qualifications;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public List<Qualification> getQualifications() {
+		return qualifications;
+	}
+
+	public void setQualifications(List<Qualification> qualifications) {
+		this.qualifications = qualifications;
+	}
 }
